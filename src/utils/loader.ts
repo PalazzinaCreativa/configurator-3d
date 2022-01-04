a28f09ffa88228a1bcd2d6e0127acd6aa30f4906
Gianluca Vedovato <gianlucs@palazzinacreativa.it>
2022-01-04 11:39:16 +0100

    new patch

--- src/utils/loader.ts
+++ src/utils/loader.ts
@@ -4,6 +4,6 @@ import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader'
 const loader = new GLTFLoader()
 const dracoLoader = new DRACOLoader()
 loader.setDRACOLoader( dracoLoader )
-dracoLoader.setDecoderPath("https://raw.githubusercontent.com/mrdoob/three.js/dev/examples/js/libs/draco/")
+dracoLoader.setDecoderPath(__dirname + "/libs/draco/")
 
 export default loader
